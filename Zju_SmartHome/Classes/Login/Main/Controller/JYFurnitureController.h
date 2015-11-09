c7fe72614e1d0bc0a79e590ab3cc01599baf4313
gujinyue1010 <361143552@qq.com>
2015-11-09 15:01:34 +0800

    家居

---
+++ Zju_SmartHome/Classes/Login/Main/Controller/JYFurnitureController.h
@@ -0,0 +1,13 @@
+//
+//  JYFurnitureController.h
+//  Zju_SmartHome
+//
+//  Created by 123 on 15/11/9.
+//  Copyright © 2015年 GJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JYFurnitureController : UIViewController
+
+@end
