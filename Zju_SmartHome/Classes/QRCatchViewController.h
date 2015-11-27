ee09a2efe078dcad205ff5925c87f11f24e2c44c
chenyufeng <503434355@qq.com>
2015-11-27 16:21:52 +0800

    完成扫码功能，界面还需优化

---
+++ Zju_SmartHome/Classes/QRCatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCatchViewController.h
+//  Zju_SmartHome
+//
+//  Created by chenyufeng on 15/11/27.
+//  Copyright © 2015年 GJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRCatchViewController : UIViewController
+
+@end
